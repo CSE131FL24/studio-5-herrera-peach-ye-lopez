7f09e0377b9e41e191f5ed1970801862cf9d912c
yamil <yamil@LAPTOP-HTL51PC4.dhcp.wustl.edu>
2024-10-17 11:18:38 -0500

    commit

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,7 +14,7 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -31,21 +31,21 @@ public class Methods {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
 
+		StdDraw.setPenColor(0,109,219);
+		StdDraw.filledCircle(x, y, 3.0/4.0 * radius);
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
 
-		
-
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledCircle(x, y, 1.0/2.0 * radius);
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
 
-		
-
+		StdDraw.setPenColor(255,255,109);
+		StdDraw.filledCircle(x, y, 1.0/4.0 * radius);
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
-		
 	}
 
 	/**
@@ -75,7 +75,10 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		int length = values.length;
+		for(int i = 0; i < length; i++) {
+			sum += values[i];
+		}
 		return sum;
 	}
 
@@ -87,8 +90,11 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int [length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
+		for(int i = 0; i < length; i++) {
+			values[i] = value;
+		}
 
 		
 
